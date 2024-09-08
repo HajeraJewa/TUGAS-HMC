3a33daae41bfa518696791bce9b9190979aaa559
HajeraJewa <hajerajewa154@gmail.com>
2024-09-08 17:37:45 +0800

    Tugas 1

---
+++ Tugas.1/index.js
@@ -0,0 +1,15 @@
+//Tugas No 1
+console.log("Hello, World!")
+//Tugas No 3
+let nama = "Hajera";
+let umur = "19 Tahun";
+let pelajar = true;
+//Tugas No 4
+const a = 10;
+const b = 5;
+const c = a + b;
+
+console.log(c);
+console.log(nama);
+console.log(umur);
+console.log(pelajar);
